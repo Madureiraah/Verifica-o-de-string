a35400a6f9d81a9d97649a5c20ef3441007082a8
Gabriel Madureira <164400637+Madureiraah@users.noreply.github.com>
2024-09-17 15:54:49 -0300

    Add files via upload

---
+++ string.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Mime;
+
+namespace atividade;
+
+class Program
+{
+    static void Main()
+    {
+        //Variáveis
+        string palavra = " ";
+        int cont = 0;
+
+        //entrada de dados
+        Console.WriteLine("Digite uma palavra: ");
+        palavra = Console.ReadLine()!;
+
+        //loop para verificar se possui letra a
+        for (int i = 0; i < palavra.Length; i++)
+        {
+            if (palavra[i] == 'a' || palavra[i] == 'A')
+            {
+                cont ++;
+            }
+        }
+
+        //saída de dados
+        if (cont == 0)
+        {
+            Console.WriteLine($"A palavra {palavra} não possui letra A em sua composição");
+        }
+        else
+        {
+            Console.WriteLine($"A palavra {palavra} possui {cont} letra A");
+        }
+    }
+}
